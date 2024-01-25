c9890d7a922c74b3a33eaf06da39d2edf375daf3
Farid Alijani <farid.alijani@gmail.com>
2024-01-26 00:08:06 +0200

    dcgan 31g 4cpus

--- myGANs/puhti_sbatch_dcgan.sh
+++ myGANs/puhti_sbatch_dcgan.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=24
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=31G
 #SBATCH --partition=gpu
 #SBATCH --time=03-00:00:00
