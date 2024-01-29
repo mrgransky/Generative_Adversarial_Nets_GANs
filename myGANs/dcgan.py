c9f37f3328d3049879e8b256b104dcaf39ac275d
Farid Alijani <farid.alijani@gmail.com>
2024-01-29 21:14:37 +0200

    run dcgan sngan using array jobs no sys.exit

--- myGANs/dcgan.py
+++ myGANs/dcgan.py
@@ -126,7 +126,7 @@ dataloader = torch.utils.data.DataLoader(
 )
 print(f"dataset contans: {len(dataset)} images | dataloader with batch_size: {opt.batchSZ}: {len(dataloader)} batches")
 #visualize(dataloader=dataloader)
-sys.exit(0)
+# sys.exit(0)
 
 def get_gen_disc_models(device: str="cuda"):
 	print(f"Generator [spectral_norm: {opt.spectralNormGen}]".center(120, "-"))
