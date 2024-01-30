5a52ea2dc0b5a71df6f88e4aa0b722f3d100f1f7
Farid Alijani <farid.alijani@gmail.com>
2024-01-30 09:28:01 +0200

    run simulateously

--- myGANs/pouta_gan.sh
+++ myGANs/pouta_gan.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+# user="`whoami`"
+# stars=$(printf '%*s' 100 '')
+# txt="$user began Slurm job: `date`"
+# ch="#"
+# echo -e "${txt//?/$ch}\n${txt}\n${txt//?/$ch}"
+# echo "${stars// /*}"
+
+# HOME_DIR=$(echo $HOME)
+# echo "HOME DIR $HOME_DIR"
+
+# datasetDIR="$HOME_DIR/datasets/sentinel2-l1c_RGB_IMGs"
+# resultsDIR="$HOME_DIR/trash_logs/GANs/misc" ########## must be adjusted! ##########
+
+# # Activate Conda environment
+# source $HOME_DIR/miniconda3/bin/activate py39
+
+# for gan_idx in 0 1
+# do
+# 	echo "GAN Method IDX: $gan_idx"
+# 	python -u gan.py \
+# 		--rgbDIR $datasetDIR \
+# 		--resDIR $resultsDIR \
+# 		--numWorkers 20 \
+# 		--lr 0.0003 \
+# 		--nepochs 50 \
+# 		--batchSZ 8 \
+# 		--ganMethodIdx $gan_idx >>$HOME_DIR/trash_logs/GANs/gan_method_$gan_idx.out 2>&1
+# done
+
+# done_txt="$user finished Slurm job: `date`"
+# echo -e "${done_txt//?/$ch}\n${done_txt}\n${done_txt//?/$ch}"
+# echo "${stars// /*}"
+
+# # Deactivate Conda environment
+# # conda deactivate
+
 user="`whoami`"
 stars=$(printf '%*s' 100 '')
 txt="$user began Slurm job: `date`"
@@ -16,18 +52,27 @@ resultsDIR="$HOME_DIR/trash_logs/GANs/misc" ########## must be adjusted! #######
 # Activate Conda environment
 source $HOME_DIR/miniconda3/bin/activate py39
 
-for gan_idx in 0 1
-do
-	echo "GAN Method IDX: $gan_idx"
-	python -u gan.py \
+# Run both commands simultaneously
+python -u gan.py \
 		--rgbDIR $datasetDIR \
 		--resDIR $resultsDIR \
 		--numWorkers 20 \
 		--lr 0.0003 \
 		--nepochs 50 \
 		--batchSZ 8 \
-		--ganMethodIdx $gan_idx >>$HOME_DIR/trash_logs/GANs/gan_method_$gan_idx.out 2>&1
-done
+		--ganMethodIdx 0 >>$HOME_DIR/trash_logs/GANs/gan_method_0.out 2>&1 &
+
+python -u gan.py \
+		--rgbDIR $datasetDIR \
+		--resDIR $resultsDIR \
+		--numWorkers 20 \
+		--lr 0.0003 \
+		--nepochs 50 \
+		--batchSZ 8 \
+		--ganMethodIdx 1 >>$HOME_DIR/trash_logs/GANs/gan_method_1.out 2>&1 &
+
+# Wait for both background jobs to finish
+wait
 
 done_txt="$user finished Slurm job: `date`"
 echo -e "${done_txt//?/$ch}\n${done_txt}\n${done_txt//?/$ch}"
